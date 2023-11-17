1a711a2563324faf563103853f8dc9b89ea39466
hush2100 <hush2100@coremethod.co.kr>
2023-11-17 17:40:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React, { useReducer, useRef } from 'react';
+
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+import New from './pages/New';
+
+
+// import MyButton from './components/MyButton';
+// import MyHeader from './components/MyHeader';
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case 'INIT' : {
+      return action.data;
+    }
+    case 'CREATE' : {
+      const newItem = {
+        ...action.data
+      }
+      newState = [newItem, ...state];
+      break;
+    }
+    case 'REMOVE' : {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT' : {
+      newState = state.map((it) => it.id === action.data.id ? {...action.data} : it)
+      break;
+    }
+    default : 
+      return state;
+  }
+  return newState;
+}
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+  //CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({type: "CREATE", data: {
+      id: dataId.current,
+      date: new Date(date).getTime(),
+      content,
+      emotion,
+    }});
+    dataId.current += 1;
+  }
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({type: "REMOVE", targetId});
+  }
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({type: "EDIT", data: {
+      id: targetId,
+      date: new Date(date).getTime(),
+      content,
+      emotion,
+    }})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <DiaryStateContext.Provider value={data}>
+    <DiaryDispatchContext.Provider value={{onCreate, onRemove, onEdit}}>
+     <BrowserRouter>
+      <div className="App">
+        <h2>App</h2>
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+    </DiaryDispatchContext.Provider>
+   </DiaryStateContext.Provider>
   );
 }
 
